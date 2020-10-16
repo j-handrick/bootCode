686506f9f0ff77de8548bbfdd93bd303b7de14c4
Jake Handrick <jake.handrick@gmail.com>
2020-10-15 22:52:53 -0500

    .

--- Portfolio/js/portfolio.js
+++ Portfolio/js/portfolio.js
@@ -1,4 +1,13 @@
+$('.theme--change').on('click', function() {
 
-$('.nav--link').on("click", function(){
-  $('.nav--link').css("font-style", "italic");
+// SHOWS WHAT 'THIS' IS IN REFERENCE TO
+console.log("---> Just click THIS elemnet with class themChange: \n", this)
+
+  if ($(this).hasClass('dark')) {
+    $('#theme').attr('href', 'css/master.css')
+  }
+  else if ($(this).hasClass('light')) {
+    $('#theme').attr('href', 'css/master2.css')
+  }
+
 });
