3093f3a7c2725ef52420621406a4c46922cbb296
Jake Handrick <jake.handrick@gmail.com>
2020-10-22 22:02:21 -0500

    A bunch of changes!!

--- Portfolio2.0/js/js.js
+++ Portfolio2.0/js/js.js
@@ -1,1 +1,12 @@
-//PRELOADER
+//SMOOTH scroll
+$(window).scroll(function(){
+console.log('scroll');
+if($(this).scrollTop() > 100) {
+      $("#dissapear").addClass('active');
+console.log("addActive")
+} else {
+    $("#dissapear").removeClass('active');
+  console.log("removeActive")
+}
+
+});
